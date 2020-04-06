64ae16bc93f08cd85b4f086040953ab2466cb320
Maria Mihai <maria.ic.mihai@gmail.com>
2020-04-06 15:25:12 +0200

    Added JSON tests for "KEBAB_CASE" property naming strategy. Added separate profile and updated the jackson property. Modified the name of the tests under BeerDtoTest class Closes #4

--- src/test/java/guru/springframework/sbmjackson/model/BeerDtoSnakeTest.java
+++ src/test/java/guru/springframework/sbmjackson/model/BeerDtoSnakeTest.java
@@ -1,18 +1,13 @@
 package guru.springframework.sbmjackson.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("snake")
 @JsonTest
 public class BeerDtoSnakeTest extends BaseTest {
-
-    @Autowired
-    ObjectMapper objectMapper;
 
     @Test
     void testSnake() throws JsonProcessingException {
--- src/test/java/guru/springframework/sbmjackson/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/sbmjackson/model/BeerDtoTest.java
@@ -1,19 +1,14 @@
 package guru.springframework.sbmjackson.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 @JsonTest
 class BeerDtoTest extends BaseTest {
 
-    @Autowired
-    ObjectMapper objectMapper;
-
     @Test
-    public void test_serializeDto() throws JsonProcessingException {
+    public void testSerializeDto() throws JsonProcessingException {
         BeerDto beerDto = getDto();
 
         String jsonString = objectMapper.writeValueAsString(beerDto);
@@ -22,7 +17,7 @@ class BeerDtoTest extends BaseTest {
     }
 
     @Test
-    public void test_deserializeToDto() throws JsonProcessingException {
+    public void testDeserializeToDto() throws JsonProcessingException {
         String jsonString = "{\"id\":\"38351e0b-37e2-4232-877a-2f36716ea197\",\"beerName\":\"Beer\",\"beerStyle\":\"ALE\",\"upc\":1234567890,\"price\":10.00,\"createdDate\":\"2020-04-06T14:31:51.99453+02:00\",\"lastUpdatedDate\":\"2020-04-06T14:31:51.9965277+02:00\"}";
 
         BeerDto beerDto = objectMapper.readValue(jsonString, BeerDto.class);
